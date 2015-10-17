c991e97aeec882cd088238de1c6ce0a16e64d7d5
WangBeyond <wangycbeyond@gmail.com>
2015-10-17 15:28:17 +0800

    fix verbose bug

--- Main.py
+++ Main.py
@@ -7,39 +7,63 @@ import time
 import logging
 
 DATA_PATH = "../resource"
-verbose = False
+verbose = True
 
-def svm_fit_to_file(train_points, train_classes, model_file):
+def svm_fit_to_file():
     start = time.time()
     with open(model_file,"wb") as handle:
-        svmManager = SvmManager(handle, verbose)
+        svmManager = SvmManager(handle, verbose=verbose)
         svmManager.fit(train_points, train_classes)
     end = time.time()
-    logging.info("training finished {0} ms").format((end - start)*1000)
+    logging.info("training finished in {0} ms".format((end - start)*1000))
 
-def svm_predict_from_file(test_points, model_file):
+def svm_predict_from_file():
     start = time.time()
     with open(model_file,"rb") as handle:
-        svmManager = SvmManager(handle, verbose)
+        svmManager = SvmManager(handle, verbose=verbose)
         svmManager.load()
         return svmManager.predict(test_points)
     end = time.time()
-    logging.info("testing finished {0} ms").format((end - start)*1000)
+    logging.info("testing finished in {0} ms".format((end - start)*1000))
 
-def log_result(txtfile, result):
-    with open(txtfile,'w') as outfile:
+def log_result(result_file, result):
+    with open(result_file,'w') as outfile:
         for i in range(0, len(result), 10):
             outfile.write(str(result[i:i+10])[1:-1]+"\n")
+
+
+def calc_error_rate():
+    with open(truth_file, 'r') as tfile, open(result_file, 'r') as pfile:
+        truth_str_list = tfile.readlines()
+        predict_str_list = pfile.readlines()
+        truth_list = ' '.join(truth_str_list).split()
+        predict_list = ' '.join(predict_str_list).split()
+        count = 0
+        for i in range(len(truth_list)):
+            count += 1 if truth_list[i] != predict_list[i] else 0
+        return count, len(truth_list), str(float(count)/len(truth_list)*100)+"%"
+
+def log_test_classes():
+    with open(truth_file, 'w') as f:
+        test_classes = obj['test_classes'][0]
+        for i in range(0, len(test_classes), 10):
+            f.write(str(test_classes[i:i+10])[1:-1]+"\n")
 
 data_file = "combined.sbow.mat"
 model_file = "../model_store/svm_model_{0}.pkl".format(data_file)
 result_file = "../output/svm_predict_{0}.txt".format(data_file)
+truth_file = "../output/svm_testclasses_{0}.txt".format(data_file)
 logging.basicConfig(filename='../log/main.log',level=logging.DEBUG, format='%(asctime)s %(message)s')
 
 obj = loadmat(join(DATA_PATH, data_file))
 train_points = obj['train_points']
 train_classes = obj['train_classes'][0]
 test_points = obj['test_points']
-svm_fit_to_file(train_points, train_classes, model_file)
-# result = svm_predict_from_file(test_points, model_file)
-# log_result(result_file, result)
+
+
+
+# svm_fit_to_file()
+result = svm_predict_from_file()
+log_result(result)
+# log_test_classes()
+# print(calc_error_rate())
--- SvmManager.py
+++ SvmManager.py
@@ -7,18 +7,19 @@ import pickle
 class SvmManager():
 
     def __init__(self, model_file, verbose=True):
-        self.clf = svm.SVC(verbose)
+        self.clf = svm.SVC(verbose=verbose)
         self.model_file = model_file
 
     def fit(self, train_list, label_list, store=True):
         self.clf.fit(train_list, label_list)
-        pickle.dump(self.clf, self.model_file)
+        if store:
+            pickle.dump(self.clf, self.model_file)
 
     def load(self):
-        self.clf2 = pickle.load(self.model_file)
+        self.clf = pickle.load(self.model_file)
 
     def predict(self, query_list):
-        return self.clf2.predict(query_list)
+        return self.clf.predict(query_list)
 
 # train_points = [[0, 0, 0], [1, 1,1], [2, 3, 0]]
 # train_classes = [0, 1, 2]
